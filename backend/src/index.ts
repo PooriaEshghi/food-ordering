4a159b135a74adde1181dd992c7a20f5117129ed
PooriaEshghi <pooriaeshghi@gmail.com>
2024-07-03 11:54:32 +0200

    implemented Routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import mongoose from 'mongoose';
+
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => console.log('Connected to MongoDB'))
 
 const app = express();
 
---
+++ frontend/src/AppRouts.tsx
@@ -0,0 +1,12 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<span>Home Page</span>} />
+      <Route path="/user-profile" element={<span>User Profile</span>} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
+};
+export default AppRoutes;
