68cf325f7cfe06b8b43cb0e9edbe4b4b2d3c681e
PooriaEshghi <pooriaeshghi@gmail.com>
2024-07-03 12:27:22 +0200

    Created Header copmponent

--- frontend/src/AppRouts.tsx
+++ frontend/src/AppRouts.tsx
@@ -1,9 +1,10 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+import Layout from "./layouts/layout";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<span>Home Page</span>} />
+      <Route path="/" element={<Layout>Home Page</Layout>} />
       <Route path="/user-profile" element={<span>User Profile</span>} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
